02662de62f291c2ac0fdf564b6cd22b24a1aec4c
Dan Frank <danielhfrank@gmail.com>
2013-02-17 12:39:38 -0500

    simple auth decorator to capture gaproxy secret header

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import common.matches
 import common.scores
 import common.users
 
 from operator import itemgetter
+from functools import wraps
 import os
 import json
 import time
@@ -20,17 +21,30 @@ import math
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
+app.config['GAPROXY_SECRET'] = os.environ.get('GAPROXY_SECRET', 'ruh roh')
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not app.debug and request.headers['X-Secret'] != app.config['GAPROXY_SECRET']:
+            # gtfo
+            return 'GTFO', 403
+        else:
+            return f(*args, **kwargs)
+    return decorated
 
 ###
 # Routing for your application.
 ###
 
 @app.route('/')
+@requires_auth
 def index():
     """Render website's home page."""
     return render_template('index.html')
 
 @app.route('/api/record_match/<player_a>:<player_b>/<int:score_a>:<int:score_b>/')
+@requires_auth
 def api_record_match(player_a, score_a, player_b, score_b):
     diff = math.abs(score_a - score_b)
     if diff > 21:
@@ -42,6 +56,7 @@ def api_record_match(player_a, score_a, player_b, score_b):
 
 @app.route('/api/leaderboard', defaults={'limit' : 10})
 @app.route('/api/leaderboard/<int:limit>')
+@requires_auth
 def api_leaderboard(limit):
     players = common.scores.get_all_players()
     recent_scores = map(common.scores.get_most_recent_score, players)
@@ -49,16 +64,19 @@ def api_leaderboard(limit):
     return json.dumps({'scores': sorted_pairs})
 
 @app.route('/api/predict_match/<player_a>:<player_b>')
+@requires_auth
 def api_predict(player_a, player_b):
     data = common.scores.get_expected_result(player_a, player_b)
     return json.dumps({'scores': dict(zip((player_a, player_b), data))})
 
 @app.route('/api/all_users', defaults={'limit' : 0})
 @app.route('/api/all_users/<int:limit>')
+@requires_auth
 def api_all_users(limit):
     return json.dumps(common.users.get_all_users(limit, app.debug))
 
 @app.route('/api/resolve_player/<player_id>')
+@requires_auth
 def api_resolve_player(player_id):
     found = common.users.get_user(player_id, app.debug)
     if found:
